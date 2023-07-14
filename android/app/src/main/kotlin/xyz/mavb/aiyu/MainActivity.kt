f5e9e66b59821ae500bca9c2551367250afdda5d
Mart van Buren <mart.van.buren@gmail.com>
2023-07-15 08:35:21 +0900

    Fix splashscreen.
    I think it broke when compileSdkVersion raised to 31.

--- android/app/src/main/kotlin/xyz/mavb/aiyu/MainActivity.kt
+++ android/app/src/main/kotlin/xyz/mavb/aiyu/MainActivity.kt
@@ -1,14 +1,6 @@
 package xyz.mavb.aiyu
 
-import android.os.Bundle
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-       // Handle the splash screen transition.
-       installSplashScreen()
-       
-       super.onCreate(savedInstanceState)
-    }
 }
