4a82c585e90947badd2ec5b336e2514101b8d693
Mart van Buren <mart.van.buren@gmail.com>
2023-09-26 10:51:54 +0900

    Integration tests for restoring account.

--- integration_test/auth_test.dart
+++ integration_test/auth_test.dart
@@ -28,5 +28,132 @@ void main() {
 
       expect(find.text("Wallet Information"), findsNothing);
     });
+
+    testWidgets("restore real account (initiate only)", (tester) async {
+      await tester.pumpWidget(await buildApp());
+      await tester.pumpAndSettle();
+
+      // Open wallet info.
+      final Finder infoButton = find.byIcon(Icons.info);
+      await tester.tap(infoButton);
+      await tester.pumpAndSettle();
+
+      // Tap restore account.
+      final Finder restoreButton = find.text("Restore Account");
+      await tester.tap(restoreButton);
+      await tester.pumpAndSettle();
+
+      // Verify that the restore account dialog is open.
+      expect(find.text("Restore Account"), findsOneWidget);
+      expect(find.text("Cancel"), findsOneWidget);
+      expect(find.text("Next"), findsOneWidget);
+
+      // Enter email and submit.
+      final Finder emailField = find.ancestor(
+        of: find.text("Email"),
+        matching: find.byType(TextField),
+      );
+      await tester.enterText(emailField, "success@simulator.amazonses.com");
+      final Finder nextButton = find.text("Next");
+      await tester.tap(nextButton);
+      await tester.pumpAndSettle();
+
+      // Ensure verification code page is shown.
+      expect(find.text("Verification Code"), findsOneWidget);
+      expect(find.text("Cancel"), findsOneWidget);
+      expect(find.text("Complete"), findsOneWidget);
+
+      // Enter (wrong) verification code and submit.
+      final Finder verificationCodeField = find.ancestor(
+        of: find.text("Verification Code"),
+        matching: find.byType(TextField),
+      );
+      await tester.enterText(verificationCodeField, "123456");
+      final Finder completeButton = find.text("Complete");
+      await tester.tap(completeButton);
+      await tester.pumpAndSettle();
+
+      // Expect error message shown.
+      expect(find.text("Invalid verification code provided, please try again."),
+          findsOneWidget);
+    });
+
+    testWidgets("restore test account (and sign out)", (tester) async {
+      int i = 0;
+      await tester.pumpWidget(await buildApp());
+      await tester.pumpAndSettle();
+
+      // Open wallet info and initiate restore.
+      final Finder infoButton = find.byIcon(Icons.info);
+      await tester.tap(infoButton);
+      await tester.pumpAndSettle();
+      final Finder restoreButton = find.text("Restore Account");
+      await tester.tap(restoreButton);
+      await tester.pumpAndSettle();
+
+      // Enter test email.
+      final Finder emailField = find.ancestor(
+        of: find.text("Email"),
+        matching: find.byType(TextField),
+      );
+      await tester.enterText(emailField, "test_user");
+      final Finder nextButton = find.text("Next");
+      await tester.tap(nextButton);
+      await tester.pumpAndSettle();
+
+      // Enter test password.
+      final Finder verificationCodeField = find.ancestor(
+        of: find.text("Verification Code"),
+        matching: find.byType(TextField),
+      );
+      await tester.enterText(
+          verificationCodeField, "012345678901234567890123456789");
+      final Finder completeButton = find.text("Complete");
+      await tester.tap(completeButton);
+      await tester.pumpAndSettle();
+
+      // Check snackbar message.
+      expect(find.text("Account restored!"), findsOneWidget);
+
+      // Wait until balance is updated.
+      i = 0;
+      while (find.text("0.0¢").evaluate().isNotEmpty) {
+        if (i++ > 5) {
+          fail("Wallet balance fetch did not get triggered on sign-in.");
+        }
+        await tester.pumpAndSettle(const Duration(seconds: 1));
+      }
+
+      // Check dialog dismissed and wallet info updated.
+      expect(find.text("Restore Account"), findsNothing);
+      expect(find.text("Wallet Balance:"), findsOneWidget);
+      expect(find.text("0.0¢"), findsNothing);
+
+      // Open wallet info and logout.
+      await tester.tap(infoButton);
+      await tester.pumpAndSettle();
+      expect(find.text("Sign Out"), findsOneWidget);
+      final Finder signOutButton = find.text("Sign Out");
+      await tester.tap(signOutButton);
+      await tester.pumpAndSettle();
+
+      // Wait until sign-out is complete.
+      i = 0;
+      while (find.text("Sign Out").evaluate().isNotEmpty) {
+        if (i++ > 10) {
+          fail("Sign out took too long.");
+        }
+        await tester.pumpAndSettle(const Duration(seconds: 1));
+      }
+      expect(find.text("Restore Account"), findsOneWidget);
+
+      // Close dialog and check wallet info updated.
+      final Finder closeButton = find.text("Close");
+      await tester.tap(closeButton);
+      await tester.pumpAndSettle();
+      expect(find.text("Wallet Information"), findsNothing);
+      expect(find.text("Wallet Balance:"), findsOneWidget);
+      expect(find.text("0.0¢"), findsOneWidget);
+    });
   });
 }
